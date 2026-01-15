284a689a7ede2539e3b78cdbf31eabd2ae60890e
Rodrigo Poda <rodrigohenrique2210@gmail.com>
2026-01-15 09:14:55 -0300

    Correção primeiro programa

--- Udemy/fundamentos1.py
+++ Udemy/fundamentos1.py
@@ -1,11 +1,7 @@
 # Criar um programa que calcule 5% de desconto
+preco = int(input('Digite o valor do produto: '))
 
-valorInt = float(input("Digite o valor a receber o desconto: "))
+desconto = int(input('Digite o desconto a ser aplicado: '))
 
-desconto = 0.05
-
-valorDesconto = valorInt * desconto
-
-valorInt = valorInt - valorDesconto
-
-print(f'O valor final é de {valorInt}, e você recebeu {valorDesconto} de desconto')
+novo_preco = preco - (preco * desconto / 100)
+print(f'O valor com desconto é de R$ {novo_preco}')
